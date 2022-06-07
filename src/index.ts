a3fe613a7ece1954142f0fb63bbded0f27472b5e
Gizmotronn <droidology@allianceofdroids.org.au>
2022-06-07 11:58:25 +0800

    👾🪱 -> [SSDROP-1] on signal-k/ -> init mongot

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Express } from 'express';
+import routes from './routes';
+
+import { config } from 'dotenv';
+config();
+
+const PORT = process.env.PORT || 3001;
+
+function createApp(): Express {
+    const app = express();
+    app.use('/api', routes);
+
+    return app;
+}
+
+async function main() {
+    console.log(`Running in ${process.env.ENVIRONMENT} mode`);
+
+    try {
+        const app = createApp();
+        app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main();
